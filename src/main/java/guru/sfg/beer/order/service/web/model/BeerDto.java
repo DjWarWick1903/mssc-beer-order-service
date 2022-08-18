61c27403381c66b907e71320c7fb923072c6b2f9
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-08-19 00:01:54 +0300

    Enhance Order Response with Beer information from Beer Service

---
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerDto.java
@@ -0,0 +1,52 @@
+package guru.sfg.beer.order.service.web.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDto implements Serializable {
+
+    static final long serialVersionUID = 1l;
+
+    @Null
+    private UUID id;
+    @Null
+    private Integer version;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @Null
+    private OffsetDateTime createdDate;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String beerName;
+
+    @NotNull
+    private String beerStyle;
+
+    @NotNull
+    private String upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @NotNull
+    @Positive
+    private BigDecimal price;
+
+    @Positive
+    private Integer quantityOnHand;
+}
