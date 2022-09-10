51531f2bee73c44831319775392b49cb6adc1d0e
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-09-10 11:04:51 +0300

    Handle validate order response

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -9,7 +9,8 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
     public static final String VALIDATE_ORDER_QUEUE = "validate-order";
-    @Bean
+    public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
+    @Bean // serialize message content to json using TextMessage
     public MessageConverter jacksonJmsMessageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
